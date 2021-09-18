542697386b9a37c81df8edfcca5cb59f2b0db7d2
hovie <hovie@DESKTOP-4764HPO>
2021-09-18 17:52:31 +0700

    Add project files.

---
+++ HelloWorld/Controllers/EXController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class EXController : Controller
+    {
+        // GET: EX
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
